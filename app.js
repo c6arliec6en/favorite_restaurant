b23e119d87494881a58458905565e44e1961662c
Charlie <chen_610146@hotmail.com>
2019-04-04 16:59:50 +0800

    Finished

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const favoriteRestaurant = require('./restaurant.json')
+const expressHandlebars = require('express-handlebars')
+
+// set static
+app.use('/', express.static('public'))
+
+//Handlebars setting
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+
+// Routing
+app.get('/', (req, res) => {
+  res.render('index', { resList: favoriteRestaurant.results })
+})
+
+app.get('/restaurants/:res_id', (req, res) => {
+  const restaurant = favoriteRestaurant.results.filter(res => res.id === Number(req.params.res_id))
+
+  res.render('show', { res: restaurant[0] })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = favoriteRestaurant.results.filter(res => {
+    return res.category.includes(keyword) || res.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  console.log(keyword, restaurants)
+  res.render('index', { resList: restaurants })
+})
+
+app.listen(port, () => {
+  console.log('Starting')
+})
