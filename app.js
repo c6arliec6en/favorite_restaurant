6ebe05bf9358f5dfe53db74c17605913bfba1544
Charlie <chen_610146@hotmail.com>
2019-04-14 21:24:23 +0800

    Add create, edit, delete function and page.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const favoriteRestaurant = require('./restaurant.json')
 const expressHandlebars = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const resData = require('./models/restaurant')
+
 
 // set static
 app.use('/', express.static('public'))
@@ -10,27 +13,112 @@ app.use('/', express.static('public'))
 //Handlebars setting
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
 
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Database error')
+})
+
+db.once('open', () => {
+  console.log('Database connected')
+  const resAll = resData.find((err, resAll) => {
+    if (err) console.error('err')
+  })
+})
 
 // Routing
 app.get('/', (req, res) => {
-  res.render('index', { resList: favoriteRestaurant.results })
+  resData.find((err, resAll) => {
+    if (err) return console.error('err')
+    res.render('index', { resList: resAll })
+  })
+
 })
 
-app.get('/restaurants/:res_id', (req, res) => {
-  const restaurant = favoriteRestaurant.results.filter(res => res.id === Number(req.params.res_id))
-
-  res.render('show', { res: restaurant[0] })
+//詳細頁面
+app.get('/restaurants/:id', (req, res) => {
+  const restaurantId = req.params.id
+  resData.findById(restaurantId, (err, restaurant) => {
+    if (err) return console.error('err')
+    res.render('detail', { res: restaurant })
+  })
 })
+
+//進入編輯頁面
+app.get('/restaurant/:id/edit', (req, res) => {
+  resData.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error('err')
+    res.render('edit', { res: restaurant })
+  })
+})
+
+//送出編輯內容
+app.post('/edit/:id', (req, res) => {
+  resData.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error('err')
+
+    restaurant.name = req.body.name
+    restaurant.category = req.body.category
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.image = req.body.image
+    restaurant.description = req.body.description
+
+    restaurant.save()
+
+    res.redirect('/restaurants/' + req.params.id)
+  })
+})
+
+//觸發刪除
+app.post('/restaurant/:id/delete', (req, res) => {
+  resData.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error('err')
+    restaurant.remove(err => {
+      if (err) return console.error(err)
+      return res.redirect('/')
+    })
+  })
+})
+
+//進入新增頁面
+app.get('/restaurant/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/new', (req, res) => {
+  // const restaurant = resData(req.body)
+
+  const restaurant = resData({
+    name: req.body.name,
+    category: req.body.category,
+    location: req.body.location,
+    phone: req.body.phone,
+    image: req.body.image,
+    description: req.body.description,
+  })
+
+  console.log(restaurant)
+  restaurant.save(err => {
+    if (err) return console.log(err)
+    return res.redirect('/')
+  })
+})
+
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = favoriteRestaurant.results.filter(res => {
-    return res.category.includes(keyword) || res.name.toLowerCase().includes(keyword.toLowerCase())
+  resData.find((err, resAll) => {
+    if (err) return console.error('err')
+    const restaurants = resAll.filter(res => {
+      return res.category.includes(keyword) || res.name.toLowerCase().includes(keyword.toLowerCase())
+    })
+    res.render('index', { resList: restaurants, keyword: keyword })
   })
-  console.log(keyword, restaurants)
-  res.render('index', { resList: restaurants, keyword: keyword })
 })
 
 app.listen(port, () => {
---
+++ models/seeds/resSeeder.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const resSchema = require('../restaurant')
+const favRes = require('./restaurant.json')
+
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('db err')
+})
+
+db.once('open', () => {
+  console.log('db connected')
+  const resData = favRes.results
+
+  // resData.forEach(restaurant => {
+  //   resSchema.create({
+  //     name: this.name,
+  //     name_en: this.name_en,
+  //     category: this.category,
+  //     image: this.image,
+  //     location: this.location,
+  //     phone: this.phone,
+  //     map: this.google_map,
+  //     rating: this.rating,
+  //     description: this.description,
+  //   })
+  // });
+
+  for (let i = 0; i < resData.length; i++) {
+    resSchema.create({
+      name: resData[i].name,
+      name_en: resData[i].name_en,
+      category: resData[i].category,
+      image: resData[i].image,
+      location: resData[i].location,
+      phone: resData[i].phone,
+      map: resData[i].google_map,
+      rating: resData[i].rating,
+      description: resData[i].description,
+    })
+  }
+})
